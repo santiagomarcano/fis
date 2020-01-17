5c546db128bea56a8e5853a533168cb1bb72ccee
santiago <smarcanoweb@gmail.com>
2020-01-17 23:05:47 +0100

    Herok

--- browserConfig.js
+++ browserConfig.js
@@ -1,10 +1,14 @@
 const chromeOptions = {
     // executablePath: process.env.CHROME_EXECUTABLE_PATH,
-    executablePath: "./node_modules/puppeteer/.local-chromium/win64-706915/chrome-win/chrome.exe",
-    headless: false,
+    // executablePath: "./node_modules/puppeteer/.local-chromium/win64-706915/chrome-win/chrome.exe",
+    headless: true,
     slowMo: 1000,
     devtools: true,
-    dataViewport: null
+    dataViewport: null,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+    ],
 }
 
 export default chromeOptions
