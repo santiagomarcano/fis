72dbb2866f20c1e23c6440dbbc1941e1004901cb
santiago <smarcanoweb@gmail.com>
2020-01-18 00:33:00 +0100

    heroku

--- browserConfig.js
+++ browserConfig.js
@@ -1,10 +1,8 @@
 const chromeOptions = {
-    // executablePath: process.env.CHROME_EXECUTABLE_PATH,
-    // executablePath: "./node_modules/puppeteer/.local-chromium/win64-706915/chrome-win/chrome.exe",
+    executablePath: process.env.CHROME_EXECUTABLE_PATH,
     headless: true,
     slowMo: 1000,
     devtools: true,
-    dataViewport: null,
     args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
